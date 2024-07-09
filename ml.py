c4f1461d0a45d70868af8bddb29120188ecebd64
BUNI Jieo <87221217+Jbox19@users.noreply.github.com>
2024-07-09 01:18:50 -0700

    Add files via upload

---
+++ ml.py
@@ -0,0 +1,12 @@
+                                    #*---Text algorithm---*
+def bs(letters):
+    print(letters)
+    l = len(letters)
+    for i in range(l):
+        for j in range(l-i-1):
+            if letters[j] > letters[j+1]:
+                letters[j], letters[j+1] = letters[j+1], letters[j]
+                print(letters)
+
+letters = ["Q", "S", "A", "M", "Z", "R"]
+bs(letters)
